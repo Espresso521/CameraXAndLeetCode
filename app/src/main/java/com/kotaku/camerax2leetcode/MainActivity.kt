d2617907003aa6d5fc4c1e3c70e292887c469cb8
Espresso521 <kotaku20220424@gmail.com>
2023-05-22 13:41:55 +0900

    1. Add take photo function; 2. Add Exif information(GPS Location) to saved picture.

--- app/src/main/java/com/kotaku/camerax2leetcode/MainActivity.kt
+++ app/src/main/java/com/kotaku/camerax2leetcode/MainActivity.kt
@@ -1,37 +1,136 @@
 package com.kotaku.camerax2leetcode
 
+import android.Manifest
+import android.content.ContentValues
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.location.Location
+import android.location.LocationManager
+import android.media.ExifInterface
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
+import android.provider.MediaStore
+import android.provider.Settings
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+import com.google.common.util.concurrent.ListenableFuture
 import com.kotaku.camerax2leetcode.databinding.ActivityMainBinding
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.ExecutorService
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        private const val TAG = "MainActivity"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf(
+                Manifest.permission.CAMERA,
+                Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+            ).apply {
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                }
+            }.toTypedArray()
+    }
+
+    private var imageCapture: ImageCapture? = null
+
+    private lateinit var cameraExecutor: ExecutorService
+    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            imageCapture = ImageCapture.Builder()
+                .build()
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageCapture)
+
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Request camera permissions
+        if (!allPermissionsGranted()) {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
+        }
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.imageCaptureButton.setOnClickListener { takePhoto() }
+
+        binding.viewFinder.apply {
+            implementationMode = PreviewView.ImplementationMode.COMPATIBLE
+        }
+        startCamera()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -50,9 +149,156 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (ContextCompat.checkSelfPermission(
+                this, Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.d(TAG, "Get Camera Permission")
+        } else {
+            Toast.makeText(
+                this, "Camera permission is needed to run this application", Toast.LENGTH_LONG
+            ).show()
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this, Manifest.permission.CAMERA
+                )
+            ) {
+                val intent = Intent()
+                intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                intent.data = Uri.fromParts("package", this.packageName, null)
+                startActivity(intent)
+            }
+        }
     }
+
+    private fun takePhoto() {
+
+        Log.d(TAG, "takePhoto start")
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
+
+        // Create time stamped name and MediaStore entry.
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.JAPAN)
+            .format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/Kotaku-CameraX-LeetCode")
+            }
+        }
+
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions
+            .Builder(
+                contentResolver,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues
+            )
+            .build()
+
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    output.savedUri?.let { uri ->
+                        Log.d(TAG, "Photo capture succeeded: $uri")
+                        getLastKnownLocation()?.let {
+                            getFileFromUri(uri)?.let { savedFile ->
+                                addLocationToPhoto(savedFile, it.latitude, it.longitude)
+                            }
+                        }
+                    }
+                }
+            }
+        )
+    }
+
+    private fun getLastKnownLocation(): Location? {
+        val mLocationManager =
+            getSystemService(LOCATION_SERVICE) as LocationManager
+        val providers = mLocationManager.getProviders(true)
+        var bestLocation: Location? = null
+        for (provider in providers) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // Permission not granted, handle accordingly
+                return null
+            }
+
+            if (!mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                // GPS provider is not enabled, handle accordingly
+                return null
+            }
+            val location = mLocationManager.getLastKnownLocation(provider) ?: continue
+
+            if (bestLocation == null || location.accuracy < bestLocation.accuracy) {
+                bestLocation = location
+            }
+        }
+        return bestLocation
+    }
+
+    private fun addLocationToPhoto(file: File, latitude: Double?, longitude: Double?) {
+        try {
+            if (latitude == null || longitude == null) return
+
+            ExifInterface(file).apply {
+                val gpsLatitudeRef = if (latitude >= 0) "N" else "S"
+                val gpsLatitude = formatLatLong(latitude)
+                val gpsLongitudeRef = if (longitude >= 0) "E" else "W"
+                val gpsLongitude = formatLatLong(longitude)
+
+                setAttribute(ExifInterface.TAG_GPS_LATITUDE, gpsLatitude)
+                setAttribute(ExifInterface.TAG_GPS_LATITUDE_REF, gpsLatitudeRef)
+                setAttribute(ExifInterface.TAG_GPS_LONGITUDE, gpsLongitude)
+                setAttribute(ExifInterface.TAG_GPS_LONGITUDE_REF, gpsLongitudeRef)
+
+                setAttribute(ExifInterface.TAG_USER_COMMENT, "Welcome to kotaku-blog.com")
+
+                saveAttributes()
+
+                Log.e(TAG, "Save attributes $gpsLatitude , $gpsLatitudeRef ; $gpsLongitude , $gpsLongitudeRef")
+            }
+
+        } catch (e: IOException) {
+            Log.e(TAG, "IOException: $e")
+        }
+    }
+
+    private fun formatLatLong(value: Double): String {
+        val degrees = value.toInt()
+        val minutes = ((value - degrees) * 60).toInt()
+        val seconds = ((value - degrees - minutes / 60.0) * 3600).toInt()
+
+        return "$degrees/1,$minutes/1,$seconds/1"
+    }
+
+    fun getFileFromUri(uri: Uri): File? {
+        val filePath: String?
+        val projection = arrayOf(MediaStore.Images.Media.DATA)
+        val cursor: Cursor? =
+            contentResolver.query(uri, projection, null, null, null)
+        cursor?.use {
+            if (it.moveToFirst()) {
+                val columnIndex: Int = it.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+                filePath = it.getString(columnIndex)
+                return File(filePath)
+            }
+        }
+        return null
+    }
+
+
 }
