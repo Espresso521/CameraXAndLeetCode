9eff9d743dfa936c5fe247b74ed2ecc2fd3ae058
Espresso521 <kotaku20220424@gmail.com>
2023-05-23 11:05:58 +0900

    1. Add leetcode 605 and 1431 solution in Unit Test

--- app/src/test/java/com/kotaku/camerax2leetcode/ExampleUnitTest.kt
+++ app/src/test/java/com/kotaku/camerax2leetcode/ExampleUnitTest.kt
@@ -1,5 +1,6 @@
 package com.kotaku.camerax2leetcode
 
+import kotlinx.coroutines.flow.flow
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -14,4 +15,86 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun leetcode1431() {
+        /**
+        There are n kids with candies. You are given an integer array candies,
+        where each candies[i] represents the number of candies the ith kid has,
+        and an integer extraCandies, denoting the number of extra candies that you have.
+        Return a boolean array result of length n, where result[i] is true if,
+        after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids,
+        or false otherwise.
+        Note that multiple kids can have the greatest number of candies.
+
+        Input: candies = [2,3,5,1,3], extraCandies = 3
+        Output: [true,true,true,false,true]
+        Explanation: If you give all extraCandies to:
+        - Kid 1, they will have 2 + 3 = 5 candies, which is the greatest among the kids.
+        - Kid 2, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+        - Kid 3, they will have 5 + 3 = 8 candies, which is the greatest among the kids.
+        - Kid 4, they will have 1 + 3 = 4 candies, which is not the greatest among the kids.
+        - Kid 5, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+         */
+        fun kidsWithCandies(candies: IntArray, extraCandies: Int): List<Boolean> {
+            val ret = MutableList(candies.size) { false }
+            val max = candies.max()
+            println("candies.size is ${candies.size}, extraCandies is $extraCandies, max is $max")
+
+            for (i in candies.indices) {
+                if (candies[i] + extraCandies >= max) {
+                    ret[i] = true
+                }
+            }
+
+            return ret
+        }
+
+        val ret = kidsWithCandies(intArrayOf(2, 3, 5, 1, 3), 3)
+        ret.forEachIndexed { i, v ->
+            println("ret[$i] = $v")
+        }
+    }
+
+    @Test
+    fun leetcode605() {
+        /**
+        You have a long flowerbed in which some of the plots are planted, and some are not.
+        However, flowers cannot be planted in adjacent plots.
+        Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty,
+        and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+        Example 1:
+        Input: flowerbed = [1,0,0,0,1], n = 1
+        Output: true
+
+        Example 2:
+        Input: flowerbed = [1,0,0,0,1], n = 2
+        Output: false
+         */
+        fun canPlaceFlowers(flowerbed: IntArray, n: Int): Boolean {
+            if (flowerbed.size == 1) {
+                return if (n > 1) false
+                else if (n == 1) return flowerbed[0] == 0
+                else return true
+            }
+            var can = 0
+            for (i in flowerbed.indices) {
+                if (flowerbed[i] == 1) continue
+                val leftEmpty = if (i == 0) true else flowerbed[i-1] == 0
+
+                val rightEmpty = if (i == flowerbed.size - 1) true else flowerbed[i+1] == 0
+
+                if (leftEmpty && rightEmpty) {
+                    flowerbed[i] = 1
+                    can++
+                }
+            }
+            return can >= n
+        }
+
+        val ret = canPlaceFlowers(intArrayOf(1,0,0,0,1), 2)
+        println("ret is $ret")
+    }
+
 }
