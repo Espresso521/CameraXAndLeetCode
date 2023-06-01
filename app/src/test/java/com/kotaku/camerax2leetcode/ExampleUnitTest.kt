41aacf4ebd64c92b06eacfa4805a66d2ad5d22fd
Espresso521 <kotaku20220424@gmail.com>
2023-06-01 11:38:48 +0900

    1. Add leetcode 1732 solution in Unit Test

--- app/src/test/java/com/kotaku/camerax2leetcode/ExampleUnitTest.kt
+++ app/src/test/java/com/kotaku/camerax2leetcode/ExampleUnitTest.kt
@@ -605,7 +605,43 @@ class ExampleUnitTest {
             return maxLength
         }
 
-        val ret = longestSubarray(intArrayOf(0,1,1,1,0,1,1,0,1))
+        val ret = longestSubarray(intArrayOf(0, 1, 1, 1, 0, 1, 1, 0, 1))
+        println("ret is $ret")
+    }
+
+    @Test
+    fun leetcode1732() {
+        /**
+        There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes.
+        The biker starts his trip on point 0 with altitude equal 0.
+        You are given an integer array gain of length n where gain[i] is the net gain in altitude
+        between points i​​​​​​ and i + 1 for all (0 <= i < n). Return the highest altitude of a point.
+
+        Example 1:
+        Input: gain = [-5,1,5,0,-7]
+        Output: 1
+        Explanation: The altitudes are [0,-5,-4,1,1,-6]. The highest is 1.
+
+        Example 2:
+        Input: gain = [-4,-3,-2,-1,4,3,2]
+        Output: 0
+        Explanation: The altitudes are [0,-4,-7,-9,-10,-6,-3,-1]. The highest is 0.
+         */
+        fun largestAltitude(gain: IntArray): Int {
+            var prix = 0
+            var max = 0
+            gain.forEach {
+                val current = prix + it
+                max = max.coerceAtLeast(current)
+                prix = current
+            }
+            return max
+        }
+
+        var ret = largestAltitude(intArrayOf(-5, 1, 5, 0, -7))
+        println("ret is $ret")
+
+        ret = largestAltitude(intArrayOf(-4,-3,-2,-1,4,3,2))
         println("ret is $ret")
     }
 
