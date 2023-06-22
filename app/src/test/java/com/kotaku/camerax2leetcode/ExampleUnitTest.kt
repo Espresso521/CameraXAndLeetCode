4fcd26cdbe623020e837293f4769eefc52caf746
Espresso521 <kotaku20220424@gmail.com>
2023-06-22 10:52:36 +0900

    1. Add leetcode 1926 solution in Unit Test

--- app/src/test/java/com/kotaku/camerax2leetcode/ExampleUnitTest.kt
+++ app/src/test/java/com/kotaku/camerax2leetcode/ExampleUnitTest.kt
@@ -1721,6 +1721,90 @@ class ExampleUnitTest {
 
     }
 
+    @Test
+    fun leetcode1926() {
+        /**
+        You are given an m x n matrix maze (0-indexed) with empty cells (represented as '.') and walls (represented as '+').
+        You are also given the entrance of the maze, where entrance = [entrancerow, entrancecol] denotes the row and column of the cell you are initially standing at.
+
+        In one step, you can move one cell up, down, left, or right. You cannot step into a cell with a wall,
+        and you cannot step outside the maze. Your goal is to find the nearest exit from the entrance.
+        An exit is defined as an empty cell that is at the border of the maze. The entrance does not count as an exit.
+
+        Return the number of steps in the shortest path from the entrance to the nearest exit, or -1 if no such path exists.
+
+        Example 1:
+        Input: maze = [["+","+",".","+"],[".",".",".","+"],["+","+","+","."]], entrance = [1,2]
+        Output: 1
+        Explanation: There are 3 exits in this maze at [1,0], [0,2], and [2,3].
+        Initially, you are at the entrance cell [1,2].
+        - You can reach [1,0] by moving 2 steps left.
+        - You can reach [0,2] by moving 1 step up.
+        It is impossible to reach [2,3] from the entrance.
+        Thus, the nearest exit is [0,2], which is 1 step away.
+
+        Example 2:
+        Input: maze = [["+","+","+"],[".",".","."],["+","+","+"]], entrance = [1,0]
+        Output: 2
+        Explanation: There is 1 exit in this maze at [1,2].
+        [1,0] does not count as an exit since it is the entrance cell.
+        Initially, you are at the entrance cell [1,0].
+        - You can reach [1,2] by moving 2 steps right.
+        Thus, the nearest exit is [1,2], which is 2 steps away.
+        */
+        fun nearestExit(maze: Array<CharArray>, entrance: IntArray): Int {
+            val directions = arrayOf(intArrayOf(1, 0), intArrayOf(-1, 0), intArrayOf(0, 1), intArrayOf(0, -1))
+            val rows = maze.size
+            val cols = maze[0].size
+
+            val queue = LinkedList<IntArray>()
+            queue.offer(entrance)
+            maze[entrance[0]][entrance[1]] = '+'
+
+            var steps = 0
+            while (queue.isNotEmpty()) {
+                val size = queue.size
+                for (i in 0 until size) {
+                    val curr = queue.poll()
+                    val row = curr[0]
+                    val col = curr[1]
+
+                    if ((row == 0 || row == rows - 1 || col == 0 || col == cols - 1) && curr != entrance) {
+                        return steps
+                    }
+
+                    for (dir in directions) {
+                        val newRow = row + dir[0]
+                        val newCol = col + dir[1]
+
+                        if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols && maze[newRow][newCol] == '.') {
+                            maze[newRow][newCol] = '+'
+                            queue.offer(intArrayOf(newRow, newCol))
+                        }
+                    }
+                }
+                steps++
+            }
+
+            return -1
+        }
+
+        //Input: maze = [["+","+",".","+"],[".",".",".","+"],["+","+","+","."]], entrance = [1,2]
+        //Output: 1
+        assertEquals(1, nearestExit(arrayOf(charArrayOf('+','+','.','+')
+            ,charArrayOf('.','.','.','+')
+            ,charArrayOf('+','+','+','.'))
+            ,intArrayOf(1,2)))
+
+        //Input: [["+","+","+"],[".",".","."],["+","+","+"]], entrance = [1,0]
+        //Output: 2
+        assertEquals(2, nearestExit(arrayOf(charArrayOf('+','+','+')
+            ,charArrayOf('.','.','.')
+            ,charArrayOf('+','+','+'))
+            ,intArrayOf(1,2)))
+
+    }
+
     private fun printTree(root: TreeNode?) {
         if (root == null) return
 
